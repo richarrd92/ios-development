6767e8234b7d61ca1be1a31f4e4a08fbaaeb9472
Richard M <160991580+richarrd92@users.noreply.github.com>
2025-03-28 16:42:18 -0400

    implement lab 5

---
+++ ios101-lab5-flix-starter-main/ios101-lab5-flix1/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  ios101-lab5-flix1
+//
+//  Created by Richard M on 3/28/25.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
